c29ff0ee67980f28736c698bda69fbe5999daa1b
Shashika Jayawardhana <shashikaj@zone24x7.com>
2025-02-08 14:12:59 +0530

    Update the column names to snake case.

--- src/repository/mysql/entities/base.entity.ts
+++ src/repository/mysql/entities/base.entity.ts
@@ -9,9 +9,9 @@ export abstract class BaseEntity extends TypeOrmBaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @CreateDateColumn()
+  @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
-  @UpdateDateColumn()
+  @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
